23d239846a8db35607d3ca03074170716e5e9f7b
nimrod216 <nimrod216@gmail.com>
2021-05-26 20:01:56 +0300

    Attacker Forward-Pass WIP with new resolution on TODO tasks

--- code/GAN.py
+++ code/GAN.py
@@ -1,4 +1,7 @@
+import math
 import time
+from pathlib import Path
+import os, sys
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
@@ -9,7 +12,6 @@ from torchvision import transforms
 import torch.nn as nn
 from torch.utils.data import DataLoader
 from datasets.twitter_dataset import TweeterDataset
-
 ##########################
 ### SETTINGS
 ##########################
@@ -52,7 +54,8 @@ optim_gener = torch.optim.Adam(model.generator.parameters(), lr=generator_learni
 optim_discr = torch.optim.Adam(model.discriminator.parameters(), lr=discriminator_learning_rate)
 
 
-
+def something():
+    raise NotImplementedError
 
 class GAN(torch.nn.Module):
     def __init__(self):
@@ -96,7 +99,8 @@ class GANTrainer:
                  att_loss_fn=binary_cross_entropy,
                  def_loss_fn=binary_cross_entropy,
                  dataset=TweeterDataset('data/'),
-                 lam=0.5
+                 lam=0.5,
+                 patience=math.inf
                 ):
         self.att_model     = att_model
         self.att_optimzer  = att_optimzer
@@ -106,11 +110,32 @@ class GANTrainer:
         self.att_loss_fn   = att_loss_fn
         self.def_loss_fn   = def_loss_fn
         self.lam           = lam
+        self.patience      = patience
 
-    def trainer(self, epochs):
+    def train(self, epochs):
+
+        #########################
+        # Define checkpoint files
+        #########################
+        checkpoint_def = "def_model_weights"
+        checkpoint_att = "att_model_weights"
+        checkpoint_def_filename = f"{checkpoint_def}{str(time.time())}.pt"
+        print(checkpoint_def)
+        Path(os.path.dirname(checkpoint_def_filename)).mkdir(exist_ok=True)
+        checkpoint_att_filename = f"{checkpoint_att}{str(time.time())}.pt"
+        print(checkpoint_att)
+        Path(os.path.dirname(checkpoint_att_filename)).mkdir(exist_ok=True)
+
+        ################################
+        # Define early stopping vraibles
+        ################################
+        patience_counter, best_val_accuracy = 0, 0
+
+
         self.start_time = time.time()
         self.discr_costs = []
         self.gener_costs = []
+
         for epoch in range(epochs):
             model = model.train()
             # for batch_idx, (features, targets) in enumerate(train_loader):
@@ -121,65 +146,98 @@ class GANTrainer:
             # valid = torch.ones(targets.size(0)).float().to(device)
             # fake = torch.zeros(targets.size(0)).float().to(device)
 
-            gener_loss, discr_loss = self.train()
+            gener_loss, discr_loss = self.foreach_epoch()
 
             self.discr_costs.append(discr_loss)
             self.gener_costs.append(gener_loss)
             # if not batch_idx % 100:
-            #     print('Epoch: %03d/%03d | Batch %03d/%03d | Gen/Dis Loss: %.4f/%.4f'
-            #           % (epoch + 1, NUM_EPOCHS, batch_idx,
-            #              len(train_loader), gener_loss, discr_loss))
+            print(f'Epoch: {epoch} | Attacker Loss: {gener_loss} | Defender loss: {discr_loss}')
+
             print('Time elapsed: %.2f min' % ((time.time() - self.start_time) / 60))
 
             print('Total Training Time: %.2f min' % ((time.time() - self.start_time) / 60))
 
+            # save weigths and preform test pass
+                # Saving attack weights
+            torch.save(self.att_model.state_dict(), checkpoint_att_filename)
+            print(
+                f"*** Saved checkpoint {checkpoint_att_filename} " f"at epoch {epoch + 1}"
+            )
+                # Saving defence weights
+            torch.save(self.def_model.state_dict(), checkpoint_def_filename)
+            print(
+                f"*** Saved checkpoint {checkpoint_def_filename} " f"at epoch {epoch + 1}"
+            )
 
-    def train(self):
+            test_result = self.test()
+
+            val_acc = test_result[0]
+            if val_acc > best_val_accuracy:
+                best_val_accuracy = val_acc
+                patience_counter = 0
+            else:
+                patience_counter += 1
+            if patience_counter >= self.patience:
+                break
+
+
+    def foreach_epoch(self):
         ### FORWARD PASS AND BACKPROPAGATION
 
         # --------------------------
-        # Train Generator
+        # Train Attacker
         # --------------------------
 
-        # Freezing Discriminator weights
+        # Freezing defender weights
         # SINGLE attacking the current network
-        attack_output = self.att_model.attack_network()
 
-        #creating tagging for loss
-        valid = somthing()
-        #TODO create the valid network with attack tagging on the new node
+        #TODO do we pass over the whole dataset as the network input?
+        attack_output = self.att_model.forward(self.dataset.data)
+
+        # creating tagging for loss
+        fake_valid = something(self.dataset.data, attack_output)
+        # TODO: create the valid network with valid tagging on the attack node
+        # and malicious tagging on the attacked node, all other are same as real tagging
 
         # Loss for fooling the GAL
-        discr_pred = self.def_model(attack_output)
+        # TODO: think if running with no_grad()?
+        # TODO: Create network with attack node and no tagging as
+        def_pred = self.def_model.forward(attack_output)
 
-        # here we use the `valid` labels because we want the discriminator to "think"
-        # the generated samples are real
-        gener_loss = self.att_loss_fn(discr_pred, valid)
+        # here we use the `valid` labels because we want the defender to "think"
+        # the attacked samples are real
+        att_loss = self.att_loss_fn(def_pred, fake_valid)
 
         self.att_optimzer.zero_grad()
-        gener_loss.backward()
+        att_loss.backward()
         self.att_optimzer.step()
 
         # --------------------------
-        # Train Discriminator
+        # Train Defender
         # --------------------------
 
-        # Freezing Generator weights
+        # Freezing Attacker weights
 
-        discr_pred_real = model.discriminator_forward(features.view(-1, IMG_SIZE))
-        real_loss = F.binary_cross_entropy(discr_pred_real, valid)
+        def_tagging_real = self.def_model.forward(self.dataset.data)
+        real_loss = self.def_loss_fn(def_tagging_real, valid)
+        #TODO generate the currect tagging for the network with attack node
+        currect_tagging_attacked = something(valid, attack_output)
 
         # here we use the `fake` labels when training the discriminator
-        discr_pred_fake = model.discriminator_forward(generated_features.detach())
-        fake_loss = F.binary_cross_entropy(discr_pred_fake, fake)
+        def_tagging_attacked = self.def_model.forward(attack_output)
+        fake_loss = self.def_loss_fn(def_tagging_attacked, currect_tagging_attacked)
 
-        discr_loss = ( (1 - self.lam) * real_loss + self.lam * fake_loss)
-        optim_discr.zero_grad()
-        discr_loss.backward()
-        optim_discr.step()
-
+        def_loss = ((1 - self.lam) * real_loss + self.lam * fake_loss)
+        self.def_optimzer.zero_grad()
+        def_loss.backward()
+        self.def_optimzer.step()
+        return att_loss, def_loss
 
     def test(self):
+        with torch.no_grad():
+            something()
+
+        return
 
 
     def evaluate(self):
