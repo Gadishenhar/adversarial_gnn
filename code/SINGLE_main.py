f038315d2e787bab5597892e2aa254368e79e27b
Gadishenhar <gadi.shenhar@gmail.com>
2021-06-05 12:22:08 +0300

    our main for the GAN training - WIP

--- code/SINGLE_main.py
+++ code/SINGLE_main.py
@@ -3,16 +3,8 @@ from classes.attack_class import AttackMode
 import sys
 from argparse import ArgumentParser
 from torch.cuda import set_device
-import torch
-from dataset_functions.graph_dataset import GraphDataset
-from datasets.twitter_dataset import TweeterDataset
-from code.model_functions import NodeModel
-from ..GAN import GANTrainer
-from GAL.models import GAL, SharedBilinearDecoder
 
-def main():
-
-    #Define parser for the arguments:
+if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument("--attMode", dest="attMode", default=AttackMode.NODE, type=AttackMode.from_string,
                         choices=list(AttackMode), required=False)
@@ -36,32 +28,10 @@ def main():
     parser.add_argument('--gpu', dest="gpu", type=int, required=False)
     parser.add_argument('--GAL', dest="GAL", type=bool, default=False, required=False)
     parser.add_argument('--GAL_gnn_type', dest="GAL_gnn_type", type=str, default="GATConv", required=False)
+
     args = parser.parse_args()
+    if args.gpu is not None:
+        set_device(args.gpu)
 
-    # define the device:
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-    # define the data set:
-    dataset = GraphDataset(DataSet.TWITTER, device)
-    num_layers = 2
-    num_of_twitter_nodes = 4972 + 2  # TODO - verify this
-    num_initial_features = dataset.num_features
-    num_final_features = dataset.num_classes
-    edges = dataset.data.edge_index.to(device)
-    hidden_dims = [32] * (num_layers - 1)
-    all_channels = [num_initial_features] + hidden_dims + [num_final_features]
-
-    # create GNNs and optimizers:
-    att_model = NodeModel(gnn_type=GNN_TYPE.GCN, num_layers=num_layers, dataset=dataset, device=device, args=args)
-    decoder = SharedBilinearDecoder(num_relations=5, num_weights=2, embed_dim=num_initial_features)
-    def_model = GAL(decoder, num_ent=num_of_twitter_nodes, embed_dim=num_initial_features, edges=edges, args=args)
-    att_adam_optimizer = torch.optim.Adam(att_model.parameters(), lr=0.0001)
-    def_adam_optimizer = torch.optim.Adam(def_model.parameters(), lr=0.0001)
-
-    # train the models:
-    trainer = GANTrainer(att_model=att_model, att_optimzer=att_adam_optimizer, def_model=def_model,
-                         def_optimzer=def_adam_optimizer, dataset=TweeterDataset('data/'))
-
-
-if __name__ == '__main__':
-    main()
+    attack = args.attMode.getAttack()
+    attack(args).run()
